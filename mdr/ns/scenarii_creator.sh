4fefc52c35ce3c18ae74f05c9334a0ebb7f4e503
netangel <netangel@5d428ead-2009-0410-835c-9c53b9a905da>
2006-02-16 23:35:21 +0000

    Passage à des var d'environnement.

--- mdr/ns/scenarii_creator.sh
+++ mdr/ns/scenarii_creator.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -x
 
-NS_HOME=~/src/ns-allinone-2.29/ns-2.29
-NTR_HOME=~/projets/adhoc
+# Mettre en variable globales NS_HOME et NTR_HOME
+#NS_HOME=~/src/ns-allinone-2.29/ns-2.29
+#NTR_HOME=~/projets/adhoc
 
 if [ $# != 7 ]
 then
@@ -19,7 +20,7 @@ RATE=$6
 PROTO=$7
 
 SCENE_FILE=$NTR_HOME/mdr/ns/mobility/scene/scen-n$NN-p0-M20-t$T-${X}x${Y}.tcl
-CBR_FILE=$NTR_HOME/mdr/ns/mobility/scene/cbr-n$($NN - 1)-s1-m$MC-r$RATE.tcl
+CBR_FILE=$NTR_HOME/mdr/ns/mobility/scene/cbr-n$(($NN - 1))-s1-m$MC-r$RATE.tcl
 #CBR_FILE=$NTR_HOME/mdr/ns/mobility/scene/cbr-n$NN-s1-m$MC-r$RATE.tcl
 
 if [ ! -r $SCENE_FILE ]
@@ -29,10 +30,10 @@ fi
 
 if [ ! -r $CBR_FILE ]
 then
-ns $NS_HOME/indep-utils/cmu-scen-gen/cbrgen.tcl -type cbr -nn $($NN - 1) -seed 1 -mc $MC -rate $RATE > $CBR_FILE
+ns $NS_HOME/indep-utils/cmu-scen-gen/cbrgen.tcl -type cbr -nn $(($NN - 1)) -seed 1 -mc $MC -rate $RATE > $CBR_FILE
 #ns $NS_HOME/indep-utils/cmu-scen-gen/cbrgen.tcl -type cbr -nn $NN -seed 1 -mc $MC -rate $RATE > $CBR_FILE
 fi
 
-$NTR_HOME/mdr/ns/test-suite-wireless-lan-dsr.tcl -x $X -y $Y -cp $CBR_FILE -sc $SCENE_FILE -nn $NN -stop $T -adhocRouting $PROTO
+$NTR_HOME/mdr/ns/wireless_last.tcl -x $X -y $Y -cp $CBR_FILE -sc $SCENE_FILE -nn $NN -stop $T -adhocRouting $PROTO
 
 
