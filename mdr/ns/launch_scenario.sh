15082a95038bec35822ec59f8e59fa236302980e
netangel <netangel@5d428ead-2009-0410-835c-9c53b9a905da>
2006-02-20 10:10:17 +0000

    + de paramètres sur le launch

--- mdr/ns/launch_scenario.sh
+++ mdr/ns/launch_scenario.sh
@@ -3,5 +3,9 @@
 TCL=wireless_antho.tcl
 PROTO=$1
 NN=$2
-NS_HOME=~/src/ns-allinone-2.29/ns-2.29 NTR_HOME=~/projets/adhoc TCL_FILE=$TCL ./scenarii_creator.sh $NN 900 800 600 10 0.0005 $PROTO
+T=10000
+X=200
+Y=200
+RATE=0.0005
+NS_HOME=~/src/ns-allinone-2.29/ns-2.29 NTR_HOME=~/projets/adhoc TCL_FILE=$TCL ./scenarii_creator.sh $NN $T $X $Y 10 $RATE $PROTO
 
